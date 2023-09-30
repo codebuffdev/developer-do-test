9f557982945824126fdba9dde0f439d5d43a317b
codebuffdev <codebuffdev@gmail.com>
2023-09-30 14:43:13 +0530

    1st unit test method

---
+++ junit-5/src/test/java/in/codebuffdev/junit5/test/annotations/TestAnnotationsDemo.java
@@ -0,0 +1,14 @@
+package in.codebuffdev.junit5.test.annotations;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestAnnotationsDemo {
+
+    @Test
+    public void test(){
+        //expected, actual
+        assertEquals(2,2);
+    }
+}
